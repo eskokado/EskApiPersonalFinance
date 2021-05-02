c3d67db8853205b63cdc82d42354c3be016ee2b4
Edson Shideki Kokado <eskokado@gmail.com>
2021-05-02 11:02:47 -0300

    Create entities in Domain

---
+++ EskApiPersonalFinance.Domain/Entities/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EskApiPersonalFinance.Domain.Entities
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+
+        public string Agency { get; set; }
+
+        public string Number { get; set; }
+
+        public decimal balance { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual User User { get; }
+
+        public IEnumerable<Launch> launches { get; }
+
+        public bool SpecialAccount(Account account)
+        {
+            return account.User.Active && DateTime.Now.Year - account.User.CreatedAt.Year >= 5; 
+        }
+    }
+}
+
---
+++ EskApiPersonalFinance.Domain/Entities/Launch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EskApiPersonalFinance.Domain.Entities
+{
+
+    public enum LaunchType
+    {
+        Income,
+        Expense
+    }
+
+    public class Launch
+    {
+        public int LaunchId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public decimal Value { get; set; }
+
+        public LaunchType LaunchType { get; set; }
+
+        public int AccountId { get; set; }
+
+        public virtual Account Account { get; }
+    }
+}
